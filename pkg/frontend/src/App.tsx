1f977a2b1b41362d1e68f62b2ff12977975f6881
Cassidy Johnson <crjohnson@legacyscape.com>
2024-08-24 13:33:09 -0500

    trying to solve ts issue in api_key declaration

--- pkg/frontend/src/App.tsx
+++ pkg/frontend/src/App.tsx
@@ -1,20 +1,22 @@
 import React, { FC } from "react";
 import "./App.css";
-import { Map, useApiIsLoaded } from "@vis.gl/react-google-maps";
+import { Map, Marker, useApiIsLoaded } from "@vis.gl/react-google-maps";
 
 const App: FC = () => {
   const isLoaded = useApiIsLoaded();
   if (!isLoaded) return <div>Loading...</div>;
-  
+
   return (
     <>
       <Map
         style={{ width: "80vw", height: "80vh" }}
-        defaultCenter={{ lat: 22.54992, lng: 0 }}
-        defaultZoom={3}
+        defaultCenter={{ lat: 30.267153, lng: -97.743057 }}
+        defaultZoom={10}
         gestureHandling={"greedy"}
         disableDefaultUI={true}
-      />
+      >
+        <Marker position={{ lat: 30.267153, lng: -97.743057 }} />
+      </Map>
     </>
   );
 };
--- pkg/frontend/src/vite-env.d.ts
+++ pkg/frontend/src/vite-env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_GOOGLE_MAPS_API_KEY: string;
-    // Add other environment variables here...
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
+  readonly VITE_GOOGLE_MAPS_API_KEY: string
+  // Add other environment variables as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
